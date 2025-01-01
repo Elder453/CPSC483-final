2579091ca743ba7bdc4302ce0df94853c4bb418f
Elder453 <elder.veliz@yale.edu>
2024-12-31 23:31:30 -0500

    update env.yml w/ TF

--- download_dataset.sh
+++ download_dataset.sh
@@ -5,17 +5,17 @@ OUTPUT_DIR="${2}/${DATASET_NAME}"
 
 BASE_URL="https://storage.googleapis.com/learning-to-simulate-complex-physics/Datasets/${DATASET_NAME}/"
 
-# Create output directory
+# create output directory
 mkdir -p ${OUTPUT_DIR}
 
-# Download all required files
+# download all required files
 echo "Downloading dataset files..."
 for file in metadata.json train.tfrecord valid.tfrecord test.tfrecord
 do
     wget -O "${OUTPUT_DIR}/${file}" "${BASE_URL}${file}"
 done
 
-# Transform each split into .pkl format
+# transform each split into .pkl format
 echo -e "\nTransforming datasets to PKL format..."
 for split in train valid test
 do
